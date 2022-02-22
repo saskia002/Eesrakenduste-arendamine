4bf60097943ac7da0d913b11a2a1695e61c060f9
Siim Kriibi <90192374+siim002@users.noreply.github.com>
2022-02-22 17:08:46 +0200

    Add files via upload

---
+++ script/clock.js
@@ -0,0 +1,62 @@
+//date time meetod
+let d = new Date();
+//console.log(d);
+
+//päevad
+let day = d.getDay();
+let days = ["Pühapäev", "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev"];
+let dayContainer = document.getElementById('day');
+dayContainer.innerHTML = days[day];
+
+//aastad
+let year = d.getFullYear();
+document.getElementById('year').innerHTML = year;
+
+//kuud
+let month = d.getMonth();
+let months = ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"];
+let monthConatiner = document.getElementById('month').innerHTML = months[month];
+
+//kuupäev
+let date = d.getDate();
+document.getElementById('date').innerHTML = date + '.';
+
+//kella aeg
+let hours = d.getHours();
+let minutes = d.getMinutes();
+let seconds = d.getSeconds();
+
+let secondsContainer = document.getElementById('seconds');
+let minutesContainer = document.getElementById('minutes');
+let hoursContainer = document.getElementById('hours');
+
+////seda asendab update blokk
+// secondsContainer.innerHTML = seconds;
+// minutesContainer.innerHTML = minutes + ':';
+// hoursContainer.innerHTML = hours+ ':';
+
+//kella uuendamine siin selle fnc sees
+function updateClock(){
+    let d = new Date();
+    hours = d.getHours();
+    minutes = d.getMinutes();
+    seconds = d.getSeconds();
+
+    //kella õige formmat ehk 00 mitte 0
+    if(hours <10){
+        hours = '0' + hours;
+    }
+    if(minutes <10){
+        minutes = '0' + minutes;
+    }
+    if(seconds < 10){
+        seconds = '0' + seconds;
+    }
+
+    secondsContainer.innerHTML = seconds;
+    minutesContainer.innerHTML = minutes + ':';
+    hoursContainer.innerHTML = hours+ ':';
+}
+
+//kella uuendamine iga teatud aja tagant, ehk teeb seda koguaeg
+window.setInterval(updateClock, 100);
